fc6619858db293c6968039767d3f11eb5295bbab
Semerak <48312228+Semerak@users.noreply.github.com>
2020-05-24 06:17:21 +0300

    Synchronizing clients dictionary

--- client3.py
+++ client3.py
@@ -1,5 +1,17 @@
 import socket
+import time
+
+from crypto import *
 from _thread import *
+
+name = input("Your name: ")
+NUMBER_BYTES = 100
+Cryp = RSA(NUMBER_BYTES, name)
+myPK = Cryp.get_pk()
+print("Key generated!")
+print("Welcome ", myPK.name)
+print("e: ", myPK.e)
+print("n: ", myPK.n)
 
 SocketServer = socket.socket()
 
@@ -14,13 +26,41 @@ except socket.error as e:
 print("Connected!")
 
 
-def wait_for_input(connection):
+def wait_for_message(connection):
     while True:
         Response = connection.recv(1024)
         print(Response.decode('utf-8'))
 
 
-start_new_thread(wait_for_input, (SocketServer,))
+
+
+SocketServer.sendall(str.encode(myPK.name))
+time.sleep(0.1)
+SocketServer.sendall(myPK.e.to_bytes(1024, byteorder='big'))
+time.sleep(0.1)
+SocketServer.sendall(myPK.n.to_bytes(1024, byteorder='big'))
+time.sleep(0.1)
+# read current clients
+clients = {}
+data = SocketServer.recv(10)
+amount_of_users = int.from_bytes(data, "big")
+time.sleep(0.1)
+for i in range(amount_of_users):
+    data = SocketServer.recv(2048)
+    name = data.decode('utf-8')
+    time.sleep(0.1)
+    data = SocketServer.recv(2048)
+    e = int.from_bytes(data, "big")
+    time.sleep(0.1)
+    data = SocketServer.recv(2048)
+    n = int.from_bytes(data, "big")
+    time.sleep(0.1)
+    clients[name] = [e, n]
+print("Now in network: ")
+#for cl in clients:
+#    print(cl)
+print(clients)
+start_new_thread(wait_for_message, (SocketServer,))
 while True:
     value = input()
     SocketServer.send(str.encode(value))
--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 import socket
 import os
 from _thread import *
+import time
 
 ServerSocket = socket.socket()
 host = '127.0.0.1'
 port = 1233
 ThreadCount = 0
 message = [""]
-clients = []
+clients = {}
+pks = {}
 lock = allocate_lock()
 try:
     ServerSocket.bind((host, port))
@@ -18,12 +20,36 @@ print('Waiting for a Connection..')
 ServerSocket.listen(5)
 
 
+def send_all_pks(connection):
+    cur_pks = pks.copy()
+    connection.sendall(len(cur_pks).to_bytes(10, byteorder='big'))
+    time.sleep(0.1)
+    for name in cur_pks:
+        connection.sendall(str.encode(name))
+        time.sleep(0.1)
+        connection.sendall(cur_pks[name][0])
+        time.sleep(0.1)
+        connection.sendall(cur_pks[name][1])
+        time.sleep(0.1)
+
+
 def threaded_client(connection, mes):
-    connection.send(str.encode('Welcome to the Server\n'))
+    # connection.send(str.encode('Welcome to the Server\n'))
+    data = connection.recv(2048)
+    name = data.decode('utf-8')
+    # name = data
+    e = connection.recv(2048)
+    n = connection.recv(2048)
+    print("New member ", name)
+    print(name, " e: ", int.from_bytes(e, "big"))
+    print(name, " n: ", int.from_bytes(n, "big"))
+    pks[name] = [e, n]
+    clients[name] = connection
+    send_all_pks(connection)
+
     while True:
 
         data = connection.recv(2048)
-
         # reply = data.decode('utf-8')
         if not data:
             break
@@ -47,9 +73,9 @@ def thread_broadcast():
         cur_mess = message[0]
         if last_mess != cur_mess:
             print(cur_mess.decode('utf-8'))
-            for connection in clients:
+            for cl in clients:
                 try:
-                    connection.sendall(cur_mess)
+                    clients[cl].sendall(cur_mess)
                 except:
                     continue
             last_mess = cur_mess
@@ -60,7 +86,6 @@ start_new_thread(thread_broadcast, ())
 while True:
     print("Searching")
     Client, address = ServerSocket.accept()
-    clients.append(Client)
     print('Connected to: ' + address[0] + ':' + str(address[1]))
     start_new_thread(threaded_client, (Client, message,))
     ThreadCount += 1
