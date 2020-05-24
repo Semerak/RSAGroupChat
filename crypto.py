b22e44542ea067ee304c890ca5605e1bd2c88c80
Semerak <48312228+Semerak@users.noreply.github.com>
2020-05-24 20:51:02 +0300

    Added comments

--- crypto.py
+++ crypto.py
@@ -1,8 +1,8 @@
-#crypto.py
+# crypto.py
 from random import randrange, getrandbits
 
 
-def is_prime(n, k=128):
+def is_prime(n, k=128):             # check if it is prime number
     if n == 2 or n == 3:
         return True
     if n <= 1 or n % 2 == 0:
@@ -27,26 +27,26 @@ def is_prime(n, k=128):
     return True
 
 
-def generate_prime_candidate(length):
+def generate_prime_candidate(length):           # generates random number with given length
     p = getrandbits(length)
     p |= (1 << length - 1) | 1
     return p
 
 
-def generate_prime_number(length=1024):
+def generate_prime_number(length=1024):         # generate prime number
     p = 4
     while not is_prime(p, 5):
         p = generate_prime_candidate(length)
     return p
 
 
-def euclid(a, q):
+def euclid(a, q):               # one step of euclid algorithm (simple)
     b = a // q
     r = a % q
     return b, r
 
 
-def pow_mod(x, y, m):
+def pow_mod(x, y, m):           # calculate pow(x,y) mod m
     binary = bin(y)[2:]
     x0 = x % m
     if binary[-1] == "1":
@@ -61,14 +61,14 @@ def pow_mod(x, y, m):
     return c
 
 
-def to_int(m_str):
+def to_int(m_str):  # function to convert str -> int
     m_int = 0
     for ch in m_str[::-1]:
         m_int = m_int * 128 + ord(ch)
     return m_int
 
 
-def to_str(m_int):
+def to_str(m_int):  # function to convert int -> str
     m_str = ""
     copy = m_int
     while copy != 0:
@@ -77,19 +77,19 @@ def to_str(m_int):
     return m_str
 
 
-def ExtendedEuclid(x, y):
+def ExtendedEuclid(x, y):           # euclid algorithm (extended)
     x0, x1, y0, y1 = 1, 0, 0, 1
     while y > 0:
         q, x, y = int(x // y), y, x % y
         x0, x1 = x1, x0 - q * x1
         y0, y1 = y1, y0 - q * y1
-    return x0, y0  # gcd and the two coefficients
+    return x0, y0  # two coefficients
 
 
-def ext_eucl_pos(x, y):
-    d = ExtendedEuclid(x, y)[1]
+def ext_eucl_pos(F, e):  # solve equation (d * e) mod F = 1
+    d = ExtendedEuclid(F, e)[1]
     if d < 0:
-        return d + x
+        return d + F
     return d
 
 
@@ -102,7 +102,7 @@ def gcd(x, y):
     return a
 
 
-class PK:
+class PK:  # class for public key
     def __init__(self, e, n, name=""):
         self.e = e
         self.n = n
@@ -111,7 +111,7 @@ class PK:
     def encrypt(self, message, coding):
         return pow_mod(coding(message), self.e, self.n)
 
-    def verify(self, message, signature):
+    def verify(self, message, signature):  # verification of the signature
         int_m = to_int(message)
         int_sig = pow_mod(signature, self.e, self.n)
         return int_m == int_sig
@@ -136,47 +136,48 @@ class RSA:
         self.__n = self.__p * self.__q
         self.__F = (self.__p - 1) * (self.__q - 1)
 
-    def add_e(self, e):
+    def add_e(self, e):             # if you want to and your own e
         if self.__e == 1:
             self.__e = e
 
-    def get_e(self):
+    def get_e(self):                # returns e (if it is not generated, this method generate it)
         if self.__e == 1:
             length = int((self.__F.bit_length() * 0.75))
             while self.__F % self.__e == 0:
                 self.__e = generate_prime_number(length)
         return self.__e
 
-    def get_pk(self):
+    def get_pk(self):               # returns object PK
         if self.__pk is None:
             self.__pk = PK(self.get_e(), self.__n, self.__name)
         return self.__pk
 
-    def __gen_d(self):
+    def __gen_d(self):              # calculate d
         e = self.get_e()
         F = self.__F
         d = ext_eucl_pos(F, e)
         return d
 
-    def __get_d(self):
+    def __get_d(self):              # returns d (if it is not generated, this method with calculate it)
         if self.__d == 1:
             self.__d = self.__gen_d()
         return self.__d
 
-    def get_name(self):
+    def get_name(self):             # returns name of this user
         return self.__name
 
-    def get_pub_key(self):
+    def get_pub_key(self):          # returns [e,n]
         return self.get_e(), self.__n
 
     def decrypt(self, c, coding):
         m = pow_mod(c, self.__get_d(), self.__n)
         return coding(m)
 
-    def sign(self, m, coding):
-        return pow_mod(to_int(m), self.__get_d(), self.__n)
+    def sign(self, m, coding):      # sign message
+        return pow_mod(coding(m), self.__get_d(), self.__n)
 
 
+# chinese remainder theorem (make some pre calculation)
 def pre_chin(m):
     M = []
     Mult_all = 1
@@ -191,6 +192,7 @@ def pre_chin(m):
     return res
 
 
+# chinese remainder theorem (use pre calculation to get result faster)
 def chin_fast(m, b):
     res = 0
     Mult_all = m[0]
@@ -200,7 +202,7 @@ def chin_fast(m, b):
     return res % Mult_all
 
 
-class Group:
+class Group:                        # class for group
     def __init__(self, *argv):
         self.__list = []
         self.__mods = []
@@ -209,7 +211,7 @@ class Group:
         self.__changed = True
         self.__update_M()
 
-    def add(self, *argv):
+    def add(self, *argv):                   # add user to group by Public Key (class PK)
         for pk in argv:
             for pki in self.__list:
                 if pki == pk:
@@ -221,16 +223,16 @@ class Group:
         self.__changed = True
         return True
 
-    def remove(self, pk):
+    def remove(self, pk):                   # remove user from group by Public Key (class PK)
         if pk in self.__list:
             self.__list.remove(pk)
             self.__mods.remove(pk.n)
             self.__changed = True
 
-    def get(self):
+    def get(self):                          # return list of PK (who is in the group)
         return self.__list.copy()
 
-    def __update_M(self):
+    def __update_M(self):                   # makes update to pre calculations for chinese theorem
         if self.__changed:
             self.__pre_M = pre_chin(self.__mods)
             self.__changed = False
